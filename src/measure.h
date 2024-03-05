071b8f55029db2a0b6a2a36ed0408955d14a6b0f
mattihirvonen <kuukkeli.matti.hirvonen@gmail.com>
2024-03-05 10:09:04 +0200

    Run only one measure task and calculate statistics

--- src/measure.h
+++ src/measure.h
@@ -23,8 +23,17 @@ class MEASURE
         int   setEfficiency( int percent );
         int   getEfficiency( void );
 
-    private:
+        int   getStat( int select );
 
+    private:
+/*
+        volatile int _mA;                // "raw" current measurement result in milli amperes
+        volatile int _mAs = 0;           // Cumulative charge in milli ampere seconds
+        volatile int _mA1s;              // Average current measurement over 1 second period
+        volatile int _Rshunt = 100000;   // micro ohm
+        volatile int _scale  = 100;      // normalize to 100
+        volatile int _efficiency = 80;   // battery charging efficiency [%]
+*/
 };
 
 #endif // _LIB_MEASURE_
