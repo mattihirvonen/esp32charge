e0419050d9452efa014d14099a35bb3e5948614b
mattihirvonen <kuukkeli.matti.hirvonen@gmail.com>
2025-09-18 18:35:20 +0300

    Fix Windows FTP scripting

--- gnuplot/plot.bat
+++ gnuplot/plot.bat
@@ -3,16 +3,15 @@
 set  DATA2GP=bin\Debug\data2gp.exe
 set  GNUPLOT=c:\pgm\gnuplot\bin\gnuplot.exe
 
-echo open 192.168.2.1   >ftpscript.txt
-echo root              >>ftpscript.txt
-echo rootpassword      >>ftpscript.txt
-echo cd  /             >>ftpscript.txt
-echo lcd .             >>ftpscript.txt
-echo get history.dat   >>ftpscript.txt
-echo bye               >>ftpscript.txt
+rem  echo open 192.168.2.1   >ftpscript.txt
+rem  echo root              >>ftpscript.txt
+rem  echo rootpassword      >>ftpscript.txt
+rem  echo cd  /             >>ftpscript.txt
+rem  echo lcd .             >>ftpscript.txt
+rem  echo get history.dat   >>ftpscript.txt
+rem  echo bye               >>ftpscript.txt
 
 ftp  -s:ftpscript.txt
-rem del     ftpscript.txt
 
 %DATA2GP%  >history.txt
 %GNUPLOT%   plot_script.gp
